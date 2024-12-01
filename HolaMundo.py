a5929a3d39fbf1b3920e9b2d29b856947fdc4489
Anekdoran <anekdoran_de_berdult@hotmail.com>
2024-12-01 22:23:08 +0100

    Hola Mundo
    Hola Mundo en Python para PIA Tarea 1

---
+++ HolaMundo.py
@@ -0,0 +1,11 @@
+# hola_mundo.py
+
+# Este programa imprime "Hola, Mundo!".
+# Autor: Luis Miguel Flores Aparicio
+# Fecha: 01/12/2024
+
+def hola_mundo():
+    print("Hola, Mundo!")
+
+if __name__ == "__main__":
+    hola_mundo()
