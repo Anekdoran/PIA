664090a333f26f5f72bf7c86934ffc025a93a7a1
Anekdoran <anekdoran_de_berdult@hotmail.com>
2024-12-19 21:33:55 +0100

    nuevos cambios en PIA

--- HolaMundo.py
+++ HolaMundo.py
@@ -4,10 +4,6 @@
 # Autor: Luis Miguel Flores Aparicio
 # Fecha: 01/12/2024
 
-def hola_mundo():
-    print("Hola, Mundo!")
-
-if __name__ == "__main__":
-    hola_mundo()
+print("Hola, Mundo!")
 
 #PRUEBA
